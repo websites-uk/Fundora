b56cf278a66258021086a9900fae5a0970674121
gracewellness.uk <gracewellnessuk@gmail.com>
2026-08-28 14:33:42 +0100

    Add supplied Fundora deposit account details

---
+++ src/config/bank-details.ts
@@ -0,0 +1,7 @@
+export const bankDetails = {
+  bankName: "Flutterwave MFB (Formerly OK MFB)",
+  accountName: "Raztune Fundora FLW",
+  accountNumber: "9432152481",
+  currency: "NGN",
+  narration: "FUNDORA + CUSTOMER NAME",
+};
